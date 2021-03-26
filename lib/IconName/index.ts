58530166112d4ef3f679b4581d13059ad49018c9
NghiNV <nghinv@lumi.biz>
2021-03-26 13:49:03 +0700

    Add typescript

---
+++ lib/IconName/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by nghinv on Thu Mar 25 2021
+ * Copyright (c) 2021 nghinv@lumi.biz
+ */
+
+export * from './AntDesign';
+export * from './Entypo';
+export * from './EvilIcons';
+export * from './Feather';
+export * from './FontAwesome';
+export * from './FontAwesome5';
+export * from './FontAwesome5Pro';
+export * from './Fontisto';
+export * from './Foundation';
+export * from './Ionicons';
+export * from './MaterialCommunityIcons';
+export * from './MaterialIcons';
+export * from './Octicons';
+export * from './SimpleLineIcons';
+export * from './Zocial';
--- react-native-icons.d.ts
+++ react-native-icons.d.ts
@@ -3,19 +3,95 @@
  * Copyright (c) 2021 nghinv@lumi.biz
  */
 
+import * as IconName from './lib/IconName';
+import { ViewStyle } from 'react-native';
 
 declare module '@nghinv/react-native-icons' {
-  import { ViewStyle } from 'react-native';
-
   export type IconType = 'AntDesign' | 'Entypo' | 'EvilIcons' | 'Feather' | 'FontAwesome' | 'FontAwesome5' | 'FontAwesome5Pro' | 'Fontisto' | 'Foundation' | 'Ionicons' | 'MaterialCommunityIcons' | 'MaterialIcons' | 'Octicons' | 'RNIMigration' | 'SimpleLineIcons' | 'Zocial';
 
-  export interface IconProps {
-    size: Number;
-    name: String;
-    color: String;
-    style: ViewStyle;
-    type: IconType
+  type AntDesignType = {
+    type?: 'AntDesign';
+    name?: IconName.AntDesignGlyphs;
   }
 
-  export function Icon(props: IconProps): JSX.Element;
+  type EntypoType = {
+    type?: 'Entypo';
+    name?: IconName.EntypoGlyphs;
+  }
+
+  type EvilIconsType = {
+    type?: 'EvilIcons';
+    name?: IconName.EvilIconsGlyphs;
+  }
+
+  type FeatherType = {
+    type?: 'Feather';
+    name?: IconName.FeatherGlyphs;
+  }
+
+  type FontAwesomeType = {
+    type?: 'FontAwesome';
+    name?: IconName.FontAwesomeGlyphs;
+  }
+
+  type FontAwesome5Type = {
+    type?: 'FontAwesome5';
+    name?: IconName.FontAwesome5Glyphs;
+  }
+
+  type FontAwesome5ProType = {
+    type?: 'FontAwesome5Pro';
+    name?: IconName.FontAwesome5ProGlyphs;
+  }
+
+  type FontistoProType = {
+    type?: 'Fontisto';
+    name?: IconName.FontistoGlyphs;
+  }
+
+  type FoundationType = {
+    type?: 'Foundation';
+    name?: IconName.FoundationGlyphs;
+  }
+
+  type IoniconsType = {
+    type?: 'Ionicons';
+    name?: IconName.IoniconsGlyphs;
+  }
+
+  type MaterialCommunityIconsType = {
+    type?: 'MaterialCommunityIcons';
+    name?: IconName.MaterialCommunityIconsGlyphs;
+  }
+
+  type MaterialIconsType = {
+    type?: 'MaterialIcons';
+    name?: IconName.MaterialIconsGlyphs;
+  }
+
+  type OcticonsType = {
+    type?: 'Octicons';
+    name?: IconName.OcticonsGlyphs;
+  }
+
+  type SimpleLineIconsType = {
+    type?: 'SimpleLineIcons';
+    name?: IconName.SimpleLineIconsGlyphs;
+  }
+
+  type ZocialType = {
+    type?: 'Zocial';
+    name?: IconName.ZocialGlyphs;
+  }
+
+  type IconTypes = AntDesignType | EntypoType | EvilIconsType | FeatherType | FontAwesomeType | FontAwesome5Type | FontAwesome5ProType | FontistoProType | FoundationType | IoniconsType | MaterialCommunityIconsType | MaterialIconsType | OcticonsType | SimpleLineIconsType | ZocialType;
+
+  export interface IconProps {
+    size?: number;
+    color?: string;
+    style?: ViewStyle;
+  }
+
+
+  export function Icon(props: IconProps & IconTypes): JSX.Element;
 }
